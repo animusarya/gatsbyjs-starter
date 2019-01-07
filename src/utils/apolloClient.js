706e88c7e05dc6860d08e14f68a6cf9bb756b2e3
Parminder Klair <perminder.klair@gmail.com>
2019-01-07 22:05:40 +0530

    build fixed

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -2,7 +2,13 @@ import ApolloClient from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import fetch from 'isomorphic-unfetch';
 import config from './config';
+
+// Polyfill fetch() on the server (used by apollo-client)
+if (!process.browser) {
+  global.fetch = fetch;
+}
 
 const httpLink = createHttpLink({
   uri: config.graphqlUri,
